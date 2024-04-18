94ddb19c76a013205efe2a2cb79ce7d87cec57d0
Murilo Augusto Maestro <murilomaestro@gmail.com>
2024-04-18 00:41:26 -0300

    refactor: extraindo tipos em alias corretos. Usando controlador de propriedades do javascript para definir propriedades;

--- src/index.ts
+++ src/index.ts
@@ -1,98 +1,111 @@
-import { DotenvConfigOptions, config } from 'dotenv';
+import { config } from 'dotenv';
 
-type Flatten<T> =
-	T extends number
-	? T
-	: T extends object
-	? { [K in keyof T]: Flatten<T[K]> }
-	: T;
-// export type Simplify<T> = {[KeyType in keyof T]: T[KeyType]} & {};
-type IfUndefinedAddString<T> = T extends undefined ? string | T : T;
+type Flatten<T> = T extends number ? T : T extends object ? {
+	[K in keyof T]: Flatten<T[K]>;
+} : T;
 
-type NewChain<Acc, K extends string, V> =
-	K extends keyof Acc
-	? Chainable<Acc & { [k in K]: Acc[k] | IfUndefinedAddString<V> }>
-	: Chainable<Acc & { [k in K]: IfUndefinedAddString<V> }>;
+type AddType<Acc> =
+	<
+		K extends string,
+		V extends string | ((v: string | undefined, ctx: AccWithoutChainOpts<Acc>) => any)
+	>
+		(key: K, value?: V) => Chainable<
+			Flatten<
+				(K extends keyof Acc ? Omit<Acc, K> : Acc) & {
+					[k in K]: V extends ((v: string | undefined, ctx: AccWithoutChainOpts<Acc>) => infer R) ? R : V | string;
+				}
+			>
+		>
 
-type ChainableOpts<Acc = {}> = {
-	add: <K extends string, V
-		extends string | ((v: string | undefined, ctx: AccWithoutChainOpts<Acc>) => any)
-	>(key: K, value?: V) => Chainable<Acc & {
-		[k in K]: V extends ((v: string | undefined, ctx: AccWithoutChainOpts<Acc>) => infer R) ? R : V | string;
-	}>;
-	inherit: <K extends string, V extends keyof Acc>(key: K, from: V) => Chainable<Acc & {
-		[k in K]: Acc[V];
-	}>;
-	remove: <K extends keyof Acc>(key: K) => Chainable<Omit<Acc, K>>;
-	render: () => Flatten<Acc>;
+type InheritConfig = {
+	quiet?: boolean;
 };
 
-type AccWithoutChainOpts<Acc> = Omit<Flatten<Acc>, keyof ChainableOpts<Flatten<Acc>>>;
+type InheritType<Acc> =
+	<
+		K extends string,
+		V extends keyof Acc
+	>
+		(key: K, from: V, config?: InheritConfig) => Chainable<
+			Flatten<
+				Acc & { [k in K]: Acc[V] | undefined; }
+			>
+		>;
 
-type Chainable<Acc = {}> = AccWithoutChainOpts<Acc> & ChainableOpts<Acc>
+type RemoveType<Acc> =
+	<K extends keyof Acc>
+		(key: K) => Chainable<Flatten<Omit<Acc, K>>>;
 
-function removeChainOpts<Acc>(chain: Acc): AccWithoutChainOpts<Acc> {
-	return Object.keys(chain as any).reduce((acc, k) => {
-		const descriptor = Object.getOwnPropertyDescriptor(chain, k);
-		if (!descriptor || (descriptor.value !== undefined)) {
-			return acc;
-		}
-		return {
-			...acc,
-			[k]: chain[k]
-		};
-	}, {} as any);
+type RenderType<Acc> = () => Flatten<Acc>;
+
+type ChainableOpts<Acc = {}> = {
+	readonly add: AddType<Acc>;
+	readonly inherit: InheritType<Acc>;
+	readonly remove: RemoveType<Acc>;
+	readonly render: RenderType<Acc>;
+};
+
+export type AccWithoutChainOpts<Acc> = Omit<Flatten<Acc>, keyof ChainableOpts<Flatten<Acc>>>;
+export type Chainable<Acc = {}> = AccWithoutChainOpts<Acc> & ChainableOpts<Acc>;
+
+function addProperty<Acc = {}>(
+	acc: Acc, 
+	key: string, 
+	config: Parameters<typeof Object.defineProperty<Acc>>[2],
+): Acc {
+	Object.defineProperty(acc, key, {
+		enumerable: true,
+		configurable: true,
+		...config,
+	});
+	return acc;
 }
 
-
-export function envChain(options?: DotenvConfigOptions): Chainable {
-	const r = config({
-		path: '.env',
-		...options,
-	})
-	console.log(r);
-	return {
+export function envChain(options: Parameters<typeof config>[0] = {
+	path: '.env',
+}): Chainable {
+	config(options);
+	const newChain: Chainable<{}> = {
 		add(key, value) {
-			let internalValue = value;
-			return {
-				...this,
-				get [key]() {
-					return typeof internalValue === 'function' ? internalValue(process.env[key], removeChainOpts(this)) :  process.env[key] ?? internalValue;
+			let internalValue = undefined as typeof value | undefined;
+			return addProperty(this, key, {
+				get() {
+					if (typeof internalValue === 'function') {
+						return internalValue(process.env[key], this);
+					}
+					if (internalValue) return internalValue;
+					if (typeof value === 'function') {
+						return value(process.env[key], this);
+					}
+					return process.env[key] ?? value;
 				},
-				set [key](v: typeof internalValue) {
+				set(v: typeof internalValue) {
 					internalValue = v;
-				}
-			}
-		},
-		inherit(key, from) {
-			return {
-				...this,
-				get [key]() {
-					return this[from];
 				},
-				set [key](v) {
-					throw new Error('Cannot set inherited value');
-				}
+			});
+		},
+		inherit(key, from, config) {
+			if (key in this) {
+				throw new Error('Cannot inherit value from existing key');
 			}
+			if (!(from in this)) {
+				throw new Error('Cannot inherit value from non-existing key');
+			}
+			return addProperty(this, key, {
+				get() {
+					if (from in this) return this[from];
+					if (config?.quiet) return undefined;
+					throw new Error('Missing inherited key');
+				},
+				set(v) {
+					if (config?.quiet) return;
+					throw new Error('Cannot set inherited value');
+				},
+			});
 		},
 		remove(key) {
-			const validKeys = Object.keys(this).filter(k => {
-				if (k === key) {
-					return false;
-				}
-				const descriptor = Object.getOwnPropertyDescriptor(this, k);
-				if (!descriptor || (descriptor.value === undefined && descriptor.set === undefined)) {
-					return false;
-				}
-				return true;
-			});
-			return {
-				...this,
-				...validKeys.reduce((acc, k) => {
-					acc[k] = this[k];
-					return acc;
-				}, {} as any)
-			};
+			delete this[key];
+			return this;
 		},
 		render() {
 			return {
@@ -106,6 +119,11 @@ export function envChain(options?: DotenvConfigOptions): Chainable {
 				}, {})
 			};
 		},
-	}
+	};
+	Object.defineProperty(newChain, 'add', { enumerable: false, writable: false, value: newChain.add });
+	Object.defineProperty(newChain, 'inherit', { enumerable: false, writable: false, value: newChain.inherit });
+	Object.defineProperty(newChain, 'remove', { enumerable: false, writable: false, value: newChain.remove });
+	Object.defineProperty(newChain, 'render', { enumerable: false, writable: false, value: newChain.render });
+	return newChain
 }
 
