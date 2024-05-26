da3b98e0d1ca91a9dee404fa606fcebbc4d00e2f
Murilo Augusto Maestro <murilomaestro@gmail.com>
2024-05-26 15:18:54 -0300

    refactor: renaming types to accomote responsabilitys, new types for flattening subchains, simpler typing in generral

--- src/index.ts
+++ src/index.ts
@@ -10,53 +10,60 @@ export type Flatten<T, Skip = undefined> =
 		: Flatten<T[K]>;
 	} : T;
 
-export type DefaultValue<ctx> =
-	string | ChainableEnv<ctx> | ((v: string | undefined, ctx: Omit<Flatten<ctx>, keyof ChainableEnvOperators<Flatten<ctx>>>) => any) | undefined;
+export type DefaultValueTo<ctx> =
+	string
+	| ChainableEnv<ctx>
+	| ((v: string | undefined, ctx: Omit<Flatten<ctx>, keyof ChainableEnvOperators<Flatten<ctx>>>) => any)
+	| undefined;
 
-type IncludeInChainWithFunction<
+export type KeysWhereChainableEnv<T> = {
+	[K in keyof T]: T[K] extends ChainableEnvOperators ? T : never
+}
+
+export type FlattenSkippingChainableEnv<T> = Flatten<T, KeysWhereChainableEnv<T>>;
+
+export type RemoveChainableEnvOperators<T> = Omit<T, keyof ChainableEnvOperators>
+
+export type AmmendInChainCtx<
 	ctx,
 	K extends string,
-	V = DefaultValue<ctx>
+	V = DefaultValueTo<ctx>
 > = (K extends keyof ctx ? Omit<ctx, K> : ctx) & {
 	[k in K]: V extends string ? string
-	: V extends ChainableEnv<infer R> ? ChainableEnv<Flatten<R>>
-	: V extends ((v: string | undefined, ctx: Omit<Flatten<ctx>, keyof ChainableEnvOperators<Flatten<ctx>>>) => infer R) ? R
+	: V extends ChainableEnvOperators<infer R> ? ChainableEnv<FlattenSkippingChainableEnv<R>>
+	: V extends ((v: string | undefined, ctx: RemoveChainableEnvOperators<FlattenSkippingChainableEnv<ctx>>) => infer R) ? R
 	: V extends undefined ? string | undefined
 	: V;
 };
 
 export type AddOperator<ctx> = <
 	K extends string,
-	V extends DefaultValue<ctx>
+	V extends DefaultValueTo<ctx>,
+	CtxAmmended = AmmendInChainCtx<ctx, K, V>
 >(key: K, defaultValue?: V) =>
 	ChainableEnv<
-		Flatten<
-			IncludeInChainWithFunction<ctx, K, V>
-		>
+		FlattenSkippingChainableEnv<CtxAmmended>
 	>;
 
 
 export type AliasOperator<ctx> = <
 	K extends string,
 	D extends string,
-	V extends DefaultValue<ctx>,
+	V extends DefaultValueTo<ctx>,
+	CtxAmmended = AmmendInChainCtx<ctx, K, V>
 >(key: K, envVariable: D, defaultValue?: V) =>
 	ChainableEnv<
-		Flatten<
-			IncludeInChainWithFunction<ctx, K, V>
-		>
+		FlattenSkippingChainableEnv<CtxAmmended>
 	>;
-
 
 export type GroupOperator<ctx> = <
 	K extends string,
 	Groupctx,
-	GroupChainableEnv = ChainableEnv<Flatten<Groupctx>>,
+	GroupChainableEnv = ChainableEnv<Flatten<Groupctx, KeysWhereChainableEnv<Groupctx>>>,
+	CtxAmmended = AmmendInChainCtx<ctx, K, GroupChainableEnv>
 >(key: K, groupCreateFunction: (envChain: ChainableEnv) => GroupChainableEnv) =>
 	ChainableEnv<
-		Flatten<
-			IncludeInChainWithFunction<ctx, K, GroupChainableEnv>
-		>
+		FlattenSkippingChainableEnv<CtxAmmended>
 	>;
 
 export type InheritConfig = {
@@ -65,20 +72,33 @@ export type InheritConfig = {
 
 export type InheritOperator<ctx> = <
 	K extends string,
-	V extends keyof ctx
+	V extends keyof ctx,
+	CtxAmmended = ctx & {
+		[k in K]: ctx[V] | undefined;
+	}
 >(key: K, from: V, config?: InheritConfig) => ChainableEnv<
-	Flatten<
-		ctx & {
-			[k in K]: ctx[V] | undefined;
-		}
-	>
+	FlattenSkippingChainableEnv<CtxAmmended>
 >;
 
-export type RemoveOperator<ctx> = <K extends keyof ctx>(key: K) => ChainableEnv<Flatten<Omit<ctx, K>>>;
+export type RemoveOperator<ctx> = <
+	K extends keyof ctx,
+	CtxAmmended = Omit<ctx, K>
+>(key: K) => ChainableEnv<
+	FlattenSkippingChainableEnv<CtxAmmended>
+>;
 
-export type RenderOperator<ctx> = () => Flatten<ctx>;
+export type RenderChainCtx<ctx> = {
+	[K in keyof ctx]: ctx[K] extends ChainableEnvOperators<infer R>
+	? RenderChainCtx<R>
+	: ctx[K]
+}
 
-export type CloneOperator<ctx> = () => ChainableEnv<Flatten<ctx>>;
+export type RenderOperator<ctx> = () =>
+	Flatten<RenderChainCtx<ctx>>;
+
+export type CloneOperator<ctx> = () => ChainableEnv<
+	Flatten<ctx, KeysWhereChainableEnv<ctx>>
+>;
 
 export type ChainableEnvOperators<ctx = {}> = {
 	readonly add: AddOperator<ctx>;
@@ -91,10 +111,7 @@ export type ChainableEnvOperators<ctx = {}> = {
 };
 
 export type ChainableEnv<ctx = {}> =
-	Omit<
-		Flatten<ctx>, 
-		keyof ChainableEnvOperators<Flatten<ctx>>
-	>
+	ctx
 	& ChainableEnvOperators<ctx>;
 
 export function envChain(options: Parameters<typeof config>[0] = {
@@ -149,7 +166,7 @@ export function envChain(options: Parameters<typeof config>[0] = {
 					if (!descriptor || (descriptor.value === undefined && descriptor.set === undefined)) {
 						return acc;
 					}
-					(acc as any)[k] = (this as any)[k];
+						(acc as any)[k] = (this as any)[k];
 					return acc;
 				}, {})
 			};
